e1482c176c1808dff312582ee8f074e537d137ed
tOñO <antvhell@hotmail.com>
2021-04-29 16:36:47 -0500

    TODOList Class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { Todo } from './classes/todo.class.js';
+
 import './styles.css';
 
-const tarea = new Todo('Aprender JavaScript');
+import { Todo, TodoList } from './classes'
 
-console.log(tarea);
+const todoList = new TodoList();
+const tarea = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('Comprar un unicornio');
+
+todoList.nuevoTodo( tarea );
+todoList.nuevoTodo( tarea2 );
+console.log(todoList);
